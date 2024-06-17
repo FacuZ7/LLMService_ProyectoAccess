ea8eb57f30909d15a573cbd28aa36636536404bf
Facundo Zapata <facundozapata41@gmail.com>
2024-06-16 22:26:40 -0300

    modularizacion y dejamos limpio el app.js

--- src/app.js
+++ src/app.js
@@ -1,49 +1,10 @@
 import { vectorStore } from './configurations/pineconeConfiguration.js'
-import { createRetrieverPrompt, createSystemPrompt } from "./utils/prompts.js";
-import { createHistoryAwareRetriever } from "langchain/chains/history_aware_retriever";
-import llm from './configurations/llm.js'
-import { ChatPromptTemplate, MessagesPlaceholder } from "@langchain/core/prompts";
-import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
-import { createRetrievalChain } from "langchain/chains/retrieval";
+import createChains from './utils/createChains.js'
 
-const retriever = vectorStore.asRetriever()
-
-const createChains = async (retriever) => {
-    const retrieverPrompt = createRetrieverPrompt();
-
-    const retrieverChain = await createHistoryAwareRetriever({
-        llm,
-        retriever,
-        rephrasePrompt: retrieverPrompt,
-    });
-
-    const system_prompt = createSystemPrompt();
-
-    const prompt = ChatPromptTemplate.fromMessages([
-        [
-            "system",
-            system_prompt,
-        ],
-        new MessagesPlaceholder("chat_history"),
-        ["user", "{input}"],
-    ]);
-
-    const chain = await createStuffDocumentsChain({
-        llm,
-        prompt
-    });
-
-    return await createRetrievalChain({
-        combineDocsChain: chain,
-        retriever: retrieverChain,
-    });
-};
-// todo ok!
-
-const conversationChain = await createChains(retriever)
+const conversationChain = await createChains(vectorStore.asRetriever())
 
 let result = await conversationChain.invoke({
-    input: "¿Cual es la cotización del dolar hoy en argentina?",
+    input: "¿Que me podes decir sobre argentina?",
 });
 
 console.log(result)
--- src/utils/getEmbedding.js
+++
@@ -1,8 +0,0 @@
-import { myEmbeddingTechnique } from "../configurations/embeddingsConfiguration.js";
-
-const getEmbedding = async (query) => {
-    const embedding = await myEmbeddingTechnique.embedQuery(query)
-    return embedding
-}
-
-export default getEmbedding;
